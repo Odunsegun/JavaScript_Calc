7a8327a0832e51dfea664047dc6c2ab1799e00c1
Oluwasegun Odunaiya <oluwasegun.odunaiya@ontariotechu.net>
2024-08-15 05:13:19 -0900

    first commit

---
+++ Calc.js
@@ -0,0 +1,49 @@
+let num1, num2, num3;
+
+
+while (true) {
+    num1 = window.prompt("Enter first number");
+    num1 = parseFloat(num1);
+    
+    if (!isNaN(num1) && num1 !== "") break;
+    alert("Please enter a valid number!");
+}
+
+while (true) {
+    num2 = window.prompt("Enter second number");
+    num2 = parseFloat(num2);
+    
+    if (!isNaN(num2) && num2 !== "") break;
+    alert("Please enter a valid number!");
+}
+
+while (true) {
+    sym1 = window.prompt("Enter an operator (+, -, *, /, %, **)");
+    
+    if (["+", "-", "*", "/", "%", "**"].includes(sym1)) break;
+    alert("Please enter a valid operator!");
+}
+
+let result;
+
+switch(sym1){
+    case "+":
+        document.write(num1 + num2);
+        break;
+    case "-":
+        document.write(num1 - num2);
+        break;  
+    case "/":
+        document.write(num1 / num2);
+        break;
+    case "*":
+        document.write(num1 * num2);
+        break;
+    case "%":
+        document.write(num1 % num2);
+        break;
+    case "**":
+        document.write(num1 ** num2);
+        break;
+}
+
