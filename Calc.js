d19459e731b1ce96de0d5787fa211cf0f6ef7344
Oluwasegun Odunaiya <oluwasegun.odunaiya@ontariotechu.net>
2024-08-21 06:14:46 -0900

    second

--- Calc.js
+++ Calc.js
@@ -1,49 +1,62 @@
-let num1, num2, num3;
+function appendToDisplay(){
+    let num1, num2, num3;
 
-
-while (true) {
-    num1 = window.prompt("Enter first number");
-    num1 = parseFloat(num1);
     
-    if (!isNaN(num1) && num1 !== "") break;
-    alert("Please enter a valid number!");
-}
 
-while (true) {
-    num2 = window.prompt("Enter second number");
-    num2 = parseFloat(num2);
+    while (true) {
+        num1 = window.prompt("Enter first number");
+        num1 = parseFloat(num1);
+        
+        if (!isNaN(num1) && num1 !== "") break;
+        alert("Please enter a valid number!");
+    }
+
+    while (true) {
+        num2 = window.prompt("Enter second number");
+        num2 = parseFloat(num2);
+        
+        if (!isNaN(num2) && num2 !== "") break;
+        alert("Please enter a valid number!");
+    }
+
+    while (true) {
+        sym1 = window.prompt("Enter an operator (+, -, *, /, %, **)");
+        
+        if (["+", "-", "*", "/", "%", "**"].includes(sym1)) break;
+        alert("Please enter a valid operator!");
+    }
+
     
-    if (!isNaN(num2) && num2 !== "") break;
-    alert("Please enter a valid number!");
-}
-
-while (true) {
-    sym1 = window.prompt("Enter an operator (+, -, *, /, %, **)");
+    document.write("<button onclick='appendToDisplay()'>Run Calculator </button>");
     
-    if (["+", "-", "*", "/", "%", "**"].includes(sym1)) break;
-    alert("Please enter a valid operator!");
-}
+    switch(sym1){
+        case "+":
+            document.write(num1 + num2);
+            break;
+        case "-":
+            document.write(num1 - num2);
+            break;  
+        case "/":
+            document.write(num1 / num2);
+            break;
+        case "*":
+            document.write(num1 * num2);
+            break;
+        case "%":
+            document.write(num1 % num2);
+            break;
+        case "**":
+            document.write(num1 ** num2);
+            break;
+    }
+    
 
-let result;
 
-switch(sym1){
-    case "+":
-        document.write(num1 + num2);
-        break;
-    case "-":
-        document.write(num1 - num2);
-        break;  
-    case "/":
-        document.write(num1 / num2);
-        break;
-    case "*":
-        document.write(num1 * num2);
-        break;
-    case "%":
-        document.write(num1 % num2);
-        break;
-    case "**":
-        document.write(num1 ** num2);
-        break;
+
+
+    
+
+            
+    
 }
 
